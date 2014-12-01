8ee676979d3351844e0a49a2301d666a61458dc3
Alain Mosnier <alain@wanamoon.net>
2014-12-01 21:26:15 +0100

    Added my standard GNU Emacs config.

---
+++ elisp/common.el
@@ -0,0 +1,102 @@
+;; My own little Emacs manual:
+;; - Show white spaces: M-x whitespace-mode
+;; - List packages: M-x list-packages
+;; - Display variable value. Example: C-h v package-archives
+
+(setq inhibit-splash-screen t)
+
+;; 
+;; Begin: CEDET config
+;;
+
+;; Load CEDET.
+;; See cedet/common/cedet.info for configuration details.
+;; IMPORTANT: Tou must place this *before* any CEDET component (including
+;; EIEIO) gets activated by another package (Gnus, auth-source, ...).
+(load-file "/home/nilo/cedet/cedet-devel-load.el")
+
+;; Add further minor-modes to be enabled by semantic-mode.
+;; See doc-string of `semantic-default-submodes' for other things
+;; you can use here.
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode t)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode t)
+(add-to-list 'semantic-default-submodes 'global-cedet-m3-minor-mode t)
+
+;; Enable Semantic
+(semantic-mode 1)
+
+;; Enable EDE (Project Management) features
+(global-ede-mode 1)
+
+(define-key cedet-m3-mode-map "\C-c " 'cedet-m3-menu-kbd)
+
+(require 'semantic/canned-configs)
+(semantic-load-enable-gaudy-code-helpers)
+
+(srecode-minor-mode 1)
+
+;; 
+;; End: CEDET config
+;;
+
+;; Load theme
+(load-theme 'wombat)
+
+;; Configure font
+(set-face-attribute 'default (selected-frame) :height 100)
+
+;; Display columns
+(column-number-mode)
+
+;; Better c-style
+(setq c-default-style "linux")
+
+;; Choose package sources
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+
+;; Smart tabs
+(add-hook 'c-mode-common-hook (lambda() (smart-tabs-insinuate 'c 'c++)))
+
+;; Keyboard shorcuts
+(define-key global-map [f4] 'goto-line)
+(define-key global-map [f5] 'compile)
+(define-key global-map [f6] 'recompile)
+(define-key global-map [f7] 'next-error)
+(define-key global-map [f8] 'magit-status)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; HACK BEGIN
+;; This hack fixes indentation for C++11's "enum class" in Emacs.
+;; http://stackoverflow.com/questions/6497374/emacs-cc-mode-indentation-problem-with-c0x-enum-class/6550361#6550361
+
+(defun inside-class-enum-p (pos)
+  "Checks if POS is within the braces of a C++ \"enum class\"."
+  (ignore-errors
+    (save-excursion
+      (goto-char pos)
+      (up-list -1)
+      (backward-sexp 1)
+      (looking-back "enum[ \t]+class[ \t]+[^}]*"))))
+ 
+(defun align-enum-class (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      0
+    (c-lineup-topmost-intro-cont langelem)))
+ 
+(defun align-enum-class-closing-brace (langelem)
+  (if (inside-class-enum-p (c-langelem-pos langelem))
+      '-
+    '+))
+ 
+(defun fix-enum-class ()
+  "Setup `c++-mode' to better handle \"class enum\"."
+  (add-to-list 'c-offsets-alist '(topmost-intro-cont . align-enum-class))
+  (add-to-list 'c-offsets-alist
+	       '(statement-cont . align-enum-class-closing-brace)))
+ 
+(add-hook 'c++-mode-hook 'fix-enum-class)
+
+;;
+;; HACK END
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
