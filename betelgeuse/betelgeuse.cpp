03f841e07948220389654438063fa5d7ecdd8d92
Alain Mosnier <alain@wanamoon.net>
2015-05-12 21:15:17 +0200

    Betelgeuse displays a balck window for 3 seconds

--- betelgeuse/betelgeuse.cpp
+++ betelgeuse/betelgeuse.cpp
@@ -15,5 +15,73 @@ int main()
 		return 1;
 	}
 
+	//Create a renderer that will draw to the window, -1 specifies that we want to load whichever
+	//video driver supports the flags we're passing
+	//Flags: SDL_RENDERER_ACCELERATED: We want to use hardware accelerated rendering
+	//SDL_RENDERER_PRESENTVSYNC: We want the renderer's present function (update screen) to be
+	//synchornized with the monitor's refresh rate
+	SDL_Renderer *ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (ren == nullptr){
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	Uint32 rmask, gmask, bmask, amask;
+
+// SDL interprets each pixel as a 32-bit number, so our masks must depend
+// on the endianness (byte order) of the machine
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	rmask = 0xff000000;
+	gmask = 0x00ff0000;
+	bmask = 0x0000ff00;
+	amask = 0x000000ff;
+#else
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = 0xff000000;
+#endif
+
+	SDL_Surface *surf = SDL_CreateRGBSurface(0, 640, 480, 32, rmask, gmask, bmask, amask);
+
+	if (surf == nullptr) {
+		std::cout << "SDL_CreateRGBSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//To use a hardware accelerated texture for rendering we can create one from
+	//the surface we loaded
+	SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, surf);
+	//We no longer need the surface
+	SDL_FreeSurface(surf);
+	if (tex == nullptr){
+		SDL_DestroyRenderer(ren);
+		SDL_DestroyWindow(win);
+		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
+		SDL_Quit();
+		return 1;
+	}
+
+	//A sleepy rendering loop, wait for 3 seconds and render and present the screen each time
+	for (int i = 0; i < 3; ++i){
+		//First clear the renderer
+		SDL_RenderClear(ren);
+		//Draw the texture
+		SDL_RenderCopy(ren, tex, NULL, NULL);
+		//Update the screen
+		SDL_RenderPresent(ren);
+		//Take a quick break after all that hard work
+		SDL_Delay(1000);
+	}
+
+	//Clean up our objects and quit
+	SDL_DestroyTexture(tex);
+	SDL_DestroyRenderer(ren);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+
 	return 0;
 }
