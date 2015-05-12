02792789507457cdd200e2d5e8eab122b68d171e
Alain Mosnier <alain@wanamoon.net>
2015-05-12 21:37:39 +0200

    Minor change

--- betelgeuse/betelgeuse.cpp
+++ betelgeuse/betelgeuse.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO) != 0){
+	if (SDL_Init(SDL_INIT_VIDEO) != 0){
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
