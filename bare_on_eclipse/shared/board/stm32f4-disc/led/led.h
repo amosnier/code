0a5e9fcbced19e7c811a2dfe038d02ab47370e49
Alain Mosnier <alain@wanamoon.net>
2016-02-04 22:50:52 +0100

    Better directory structure for share files

---
+++ bare_on_eclipse/shared/board/stm32f4-disc/led/led.h
@@ -0,0 +1,12 @@
+#ifndef LED_H
+#define LED_H
+
+#include "stm32f4_discovery.h"
+
+#define LED_ORANGE LED3
+#define LED_GREEN  LED4
+#define LED_RED    LED5
+#define LED_BLUE   LED6
+
+
+#endif // LED_H
