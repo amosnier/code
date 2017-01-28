55a9bdbb1219fbd26278784b2cda641fd9e439c1
Alain Mosnier <alain@wanamoon.net>
2017-01-28 10:41:25 +0100

    Started state machine implementation for console_commands

--- stm32f746-disco/application/console_commands.c
+++ stm32f746-disco/application/console_commands.c
@@ -3,15 +3,42 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool command_received = false;
+static bool command_received = false;
+
+static enum {
+	STATE_IDLE,
+	STATE_STOPWATCH,
+} state = STATE_IDLE;
+
+static void state_idle(void)
+{
+	if (!command_received)
+		return;
+
+	printf("\r\n\"%s\" is not a supported command\r\n", console_command());
+	command_received = false;
+	console_command_handled();
+}
+
+static void state_stopwatch(void)
+{
+
+}
 
 void console_command_step(void)
 {
-	if (command_received) {
-		printf("\r\nYou have entered \"%s\"\r\n", console_command());
-		command_received = false;
-		console_command_handled();
+	switch (state) {
+	case STATE_IDLE:
+		state_idle();
+		break;
+	case STATE_STOPWATCH:
+		state_stopwatch();
+		break;
+	default:
+		assert(false);
+		break;
 	}
+
 }
 
 void console_command_received(void)
