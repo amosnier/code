989b18ee6cfa6b20d5cd6bc593a2cc201c8c1a0c
Alain Mosnier <alain@wanamoon.net>
2019-09-01 20:09:16 +0200

    Added start position in Python trivial ballistics

--- python_code/ballistics_0.py
+++ python_code/ballistics_0.py
@@ -1,14 +1,17 @@
 import numpy
 import pylab
 
-t = numpy.linspace(0, 2.1, 100)
+t = numpy.linspace(0, 2.2, 100)
 
-vx = 500
-vy = 10
+v0x = 500
+v0y = 10
 g = 9.81
 
-x = vx * t
-y = vy * t - g * t ** 2 / 2
+x0 = 0
+y0 = 1.5
+
+x = x0 + v0x * t
+y = y0 + v0y * t - g * t ** 2 / 2
 
 pylab.plot(x, y)
 pylab.show()
